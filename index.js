45d815f7a5e897fe9fefde371ced5fd89a1c5108
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-11 16:18:48 +0200

    Add append and prepend functions

--- index.js
+++ index.js
@@ -6,5 +6,39 @@ class Node {
 }
 
 class LinkedList {
-    constructor() { }
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        if (this.head === null) {
+            this.prepend(value);
+        } else {
+            let node = this.head;
+            while (node.nextNode !== null) {
+                node = node.nextNode;
+            }
+            node.nextNode = new Node(value);
+        }
+    }
+
+    prepend(value) {
+        if (this.head === null) {
+            this.head = new Node(value);
+        } else {
+            const currentHead = this.head;
+            this.head = new Node(value);
+            this.head.nextNode = currentHead;
+        }
+    }
 }
+
+const list = new LinkedList();
+
+list.prepend('1');
+list.prepend('2');
+list.prepend('3');
+
+list.append('6');
+
+console.log(list);
